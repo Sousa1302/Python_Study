eb4c09ec9c094d14730cb5f6593aebc3d426b12a
Sousa1302 <sousaraposo1302@gmail.com>
2024-06-25 09:44:18 +0000

    Practice

---
+++ Tutorial_Classes/4/exercise.py
@@ -0,0 +1,29 @@
+def operation():
+    sum = (15 + 30) / 2
+    print(f"The total is: {sum}")
+
+
+num1 = 15
+num2 = 5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+print(num1 % num2)
+
+name = "Goncalo"
+food1, food2, food3 = "Alface", "Cenoura", "Tomate"
+
+print("Hello" * 10)
+
+name, age = "Sousa", 17
+
+surname1 = "sousa"
+surname2 = "raposo"
+surname3 = surname2 + " " + surname1
+
+print(surname1[3])
+print(surname2[0:6])
+print(surname3[0:])     #PRINTS THE WHOLE SENTENCE
+
