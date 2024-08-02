5124d93b21c9f570564aca1deb9c444d45864d9f
Sousa1302 <sousaraposo1302@gmail.com>
2024-08-02 15:23:49 +0000

    To do List app Done

--- Projects/ToDoList/main.py
+++ Projects/ToDoList/main.py
@@ -6,17 +6,21 @@ import os
 task_id = []
 tasks_list = []
 state_task = []
-num_id = 0
+current_num_id = 0
+next_num_id = 0
 
 def add():
-    global num_id
+    global current_num_id
+    global next_num_id
+
     task = input("Type your task: ")
     tasks_list.append(task)
     state_task.append("Not Done")
-    task_id.append(num_id)
-    num_id += 1
+    task_id.append(current_num_id)
+    next_num_id = current_num_id + 1 
+    current_num_id = next_num_id
 
-#Problem here (it doesnt recognize the id given)
+
 def delete():
     whichtask = int(input("ID of the task you want to delete: "))
 
@@ -24,6 +28,8 @@ def delete():
         del tasks_list[whichtask]
         del state_task[whichtask]
         del task_id[whichtask]
+        print("The task was successfully deleted !\n")
+        time.sleep(2)
     else:
         print("No task found for the ID provided !\n")
 
@@ -32,40 +38,56 @@ def markdone():
     whichtask = int(input("ID of the task you want to mark as done: "))
 
     if whichtask <= len(task_id) and whichtask > 0:
-        state_task[whichtask] = "Done"
+        state_task[whichtask] = "Done    "
 
-# Problem here ( The id keeps adding so all the tasks have the same id)
+
 def view():
-    global num_id
-
+    
     print("-----------------------------------------------")
     print("| ID  |       Task        |       State       |")
     print("-----------------------------------------------")
 
     for x in range(0, len(tasks_list)):
-        print(f"| {num_id} | {tasks_list[x]}             |      {state_task[x]}     |")
-        
+        print(f"|  {task_id[x]}  | {tasks_list[x]}             |      {state_task[x]}     |")
+    print("-----------------------------------------------")
+    print("\n\n")
+
 
 def mainMenu():
     print("Welcome to the To do List Application !\n")
 
     while True:
         print("What do you wish to do ?")
-        choice = int(input("1. Add a Task \n2. Delete a Task \nView all Taskes \nMark a Task as Done \n"))
+        choice = int(input("1. Add a Task \n2. Delete a Task \n3. View all Taskes \n4. Mark a Task as Done \n5. Exit \n"))
 
-         
+        os.system('clear')
 
+        if choice == 1:
+            add()
+            os.system('clear')            
+
+        elif choice == 2:
+            view()
+            delete()
+            os.system('clear')
+
+        elif choice == 3:
+            view()
+
+        elif choice == 4:
+            view()
+            markdone()
+            os.system('clear')
+
+        elif choice == 5:
+            break
+        else:
+            print("You must type a number between 1-4 !\n")
+            time.sleep(1)
+            os.system('clear')
 
 def main():
-    add()
-    #time.sleep(1)
-    #os.system('clear')
-    view()
-    add()
-    #markdone()
-    view()
-    delete()
-
+    mainMenu()
     
 
 if __name__ == "__main__":
