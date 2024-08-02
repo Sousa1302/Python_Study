e02b579ebd77dffaaf75ef27ba1765ec691efd82
Sousa1302 <sousaraposo1302@gmail.com>
2024-08-02 15:38:13 +0000

    Try and except

---
+++ Tutorial_Classes/11/class.py
@@ -0,0 +1,14 @@
+# Try and Except 
+# valuable tool when errors or exceptions are possible
+
+# He tries the block of code normally but if theres an error
+# like comparing an integer with a string then it jumps to the except
+
+try:
+    if "ola" > 3:
+        print("Hello")
+except:
+    print("An error was detected in your code !\n")
+
+
+
---
+++ Tutorial_Classes/11/exercise.py
@@ -0,0 +1,9 @@
+
+try:
+    num_1 = int(input("Type a num: "))
+    num_2 = int(input("Type a num: "))
+    
+    num_3 = num_1 / num_2
+    print(num_3)
+except:
+    print("There was a error !")
